c36b5c368581a71da9cc583010b2a824533e11aa
Jakub Blaszczyk | hybris <jakub.blaszczyk@hybris.com>
2016-07-14 21:18:03 +0200

    first draft of chef run

---
+++ files/client.rb
@@ -0,0 +1,12 @@
+log_level          :info
+log_location       STDOUT
+ssl_verify_mode    :verify_none
+chef_server_url    "https://chef.pitux.ovh"
+Mixlib::Log::Formatter.show_time = true
+file_cache_path    "/var/cache/chef"
+file_backup_path   "/var/lib/chef/backup"
+pid_file           "/var/run/chef/client.pid"
+cache_options({ :path => "/var/cache/chef/checksums", :skip_expires => true})
+signing_ca_user "chef"
+Mixlib::Log::Formatter.show_time = true
+validation_client_name "chef-validator"
