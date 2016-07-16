55c02442995eb719aa37e3399853d05c47df2da3
Jakub Blaszczyk | hybris <jakub.blaszczyk@hybris.com>
2016-07-16 21:11:24 +0200

    modified chef config

--- files/client.rb
+++ files/client.rb
@@ -3,6 +3,7 @@ log_location       STDOUT
 ssl_verify_mode    :verify_none
 chef_server_url    "https://chef.pitux.ovh"
 Mixlib::Log::Formatter.show_time = true
+node_name          "raspberry"
 file_cache_path    "/var/cache/chef"
 file_backup_path   "/var/lib/chef/backup"
 pid_file           "/var/run/chef/client.pid"
