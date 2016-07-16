2a90a408efb9425b7d080267c5215954ff7c169b
Jakub Blaszczyk | hybris <jakub.blaszczyk@hybris.com>
2016-07-16 15:32:30 +0200

    set chef to manual install

--- scripts/software/get_chef.sh
+++ scripts/software/get_chef.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -x
-echo "%%%%%%%%%%%%%%%%%%%% Installing the Chef package %%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+echo "----> Installing the Chef package"
 sudo DEBIAN_FRONTEND=noninteractive apt-get -fuy -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" --force-yes install chef
-echo "%%%%%%%%%%%%%%%%%%%%%%%% Downloading TihghVNCServer for remoteaccess %%%%%%%%%%%%%%%%%%%" 
-sudo apt-get -fuy install tightvncserver
-echo "%%%%%%%%%%%%%%%%%%%%%%%%  CleanUp after installing  %%%%%%%%%%%%%%%%%%%%%%"
+echo "----> CleanUp after installing"
 sudo apt-get -y autoremove
 sudo apt-get -y autoclean
